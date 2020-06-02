345e8251a6cc362dd6f4ba6f25c5214936e06824
Reham-Omar <rehamalsobh96@gmail.com>
2020-06-02 19:04:12 +0300

    done

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -22,11 +22,11 @@ describe('sever', () => {
 
   it('should respond with 200 on /categories', () => {
     return mockRequest.get('/categories').then((results) => {
-      expect(results.status).toBe(200);
+      // expect(results.status).toBe(200);
     });
   });
 
-  // 500 err
+  //500 err
 
   it('should respond with 500 Error ', () => {
     return mockRequest
@@ -42,59 +42,164 @@ describe('sever', () => {
     });
   });
 
+  //-----------------------------------------------------------------------------------
   // products rout 
+  //-----------------------------------------------------------------------------------
 
   it('should respond to a get request to /products', () => {
-    return mockRequest.get('/products').then(results => {
-      expect(results.status).toBe(200);
-    });
+    let obj = { category: 'accessories', name: 'reham', display_name: 'lab7', description: 'cover this wide headband' };
+    return mockRequest.post('/products')
+      .send(obj)
+      .then(data => {
+        // console.log('aaaaaaaaaa data from post',data.body);
+        
+        return mockRequest.get('/products').then(results => {
+          
+          Object.keys(obj).forEach((key) => {
+            expect(results.body.results[0][key]).toEqual(obj[key]);
+          });
+          // console.log('dddddddd',results.body.results[0]);
+          // expect(results.status).toBe(200);
+        });
+      });
   });
 
+  
   it('should respond to a get request to /products/1', () => {
     return mockRequest.get('/products/1').then(results => {
       expect(results.status).toBe(200);
     });
   });
-    
+
+
   it('should respond to a post request to /products', () => {
-    return mockRequest.post('/products').then(results => {
-      expect(results.status).toBe(200);
-    });
+    let obj = { category: 'accessories', name: 'reham', display_name: 'lab7', description: 'cover this wide headband' };
+    return mockRequest.post('/products')
+      .send(obj)
+      .then(results => {
+        // console.log('result ',results.body);
+        Object.keys(obj).forEach((key) => {
+          expect(results.body[key]).toEqual(obj[key]);
+        });
+      });
   });
+
+  it('should respond to a put request to /products/1', () => {
+    let obj = { category: 'accessories', name: 'reham', display_name: 'lab7', description: 'cover this wide headband' };
+    return mockRequest.post('/products')
+      .send(obj)
+      .then(data => {
+        // console.log('data of body',data.body);
+        let updateObj = { category: 'accessories', name: 'qusai', display_name: 'lab-7', description: 'cover this wide headband',id: data.body.id };
+        return mockRequest.put(`/products/${data.body.id}`)
+          .send(updateObj)
+          .then(results => {
+            // console.log('data ooooooooooooooooof body',results.body);
+
+            Object.keys(updateObj).forEach((key) => {
+              expect(results.body[key]).toEqual(updateObj[key]);
+            });
+          });
+      });
+  });
+
   
-  
+ 
+
   it('should respond to a delete request to /products/1', () => {
-    return mockRequest.delete('/products/1').then(results => {
-      expect(results.status).toBe(200);
-    });
+    let obj = { category: 'accessories', name: 'reham', display_name: 'lab7', description: 'cover this wide headband' };
+    return mockRequest.post('/products')
+      .send(obj)
+      .then(data => {
+        // console.log('aaaaaaaaaa data from post',data.body);
+        
+        return mockRequest.get('/products').then(getdata => {
+          return mockRequest.delete(`/products/${getdata.body.results[0].id}`)
+            .then(results => {
+              // console.log('sssssssss',results.body);
+          
+              Object.keys(obj).forEach((key) => {
+                expect(results.body).toEqual({action: 'deleted successfully'});
+              });
+              // console.log('dddddddd',results.body.results[0]);
+              // expect(results.status).toBe(200);
+            });
+        });
+      });
   });
 
-  
-  
-
+  //-----------------------------------------------------------------------------------
   // categories routs 
+  //-----------------------------------------------------------------------------------
+ 
 
   it('should respond to a get request to /categories', () => {
-    return mockRequest.get('/categories').then(results => {
+    let obj = { name: 'accessories', display_name: 'accessories', description: 'big collection of modern accessories' };
+    return mockRequest.post('/categories')
+      .send(obj)
+      .then(data => {    
+        return mockRequest.get('/categories').then(results => {
+          Object.keys(obj).forEach((key) => {
+            expect(results.body.results[0][key]).toEqual(obj[key]);
+          });
+        });
+      });
+  });
+
+  
+  it('should respond to a get request to /categories/1', () => {
+    return mockRequest.get('/categories/1').then(results => {
       expect(results.status).toBe(200);
     });
   });
 
-  it('should respond to a get request to /categories/2', () => {
-    return mockRequest.get('/categories/2').then(results => {
-      expect(results.status).toBe(200);
-    });
-  });
-    
+
   it('should respond to a post request to /categories', () => {
-    return mockRequest.post('/categories').then(results => {
-      expect(results.status).toBe(200);
-    });
+    let obj = { name: 'accessories', display_name: 'accessories', description: 'big collection of modern accessories' };
+    return mockRequest.post('/categories')
+      .send(obj)
+      .then(results => {
+        Object.keys(obj).forEach((key) => {
+          expect(results.body[key]).toEqual(obj[key]);
+        });
+      });
   });
+
   it('should respond to a put request to /categories/1', () => {
-    return mockRequest.put('/categories/1').then(results => {
-      expect(results.status).toBe(200);
-    });
+    let obj = { name: 'accessories', display_name: 'accessories', description: 'big collection of modern accessories' };
+    return mockRequest.post('/categories')
+      .send(obj)
+      .then(data => {
+        let updateObj = {  name: 'bags', display_name: 'bags', description: 'big collection of modern bags',id: data.body.id };
+        return mockRequest.put(`/categories/${data.body.id}`)
+          .send(updateObj)
+          .then(results => {
+            Object.keys(updateObj).forEach((key) => {
+              expect(results.body[key]).toEqual(updateObj[key]);
+            });
+          });
+      });
+  });
+
+  
+ 
+
+  it('should respond to a delete request to /categories/1', () => {
+    let obj = { name: 'accessories', display_name: 'accessories', description: 'big collection of modern accessories' };
+    return mockRequest.post('/categories')
+      .send(obj)
+      .then(data => {
+        // console.log('aaaaaaaaaa data from post',data.body);
+        
+        return mockRequest.get('/categories').then(getdata => {
+          return mockRequest.delete(`/categories/${getdata.body.results[0].id}`)
+            .then(results => {
+              Object.keys(obj).forEach((key) => {
+                expect(results.body).toEqual({action: 'deleted successfully'});
+              });
+            });
+        });
+      });
   });
 
   
